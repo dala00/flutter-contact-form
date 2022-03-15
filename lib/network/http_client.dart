3af7d2bf4259cb9cfffe9a433c0a5224a24d895e
Shoichi Namba <alphabrend@gmail.com>
2022-03-15 09:59:14 +0900

    Change token

--- lib/network/http_client.dart
+++ lib/network/http_client.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:crypto/crypto.dart';
 import 'package:http/http.dart' as http;
 
 class HttpClient {
@@ -37,9 +38,26 @@ class HttpClient {
   }
 
   Map<String, String> createHeaders() {
+    final token = createToken();
+
     return {
-      'Authorization': 'Bearer $applicationKey',
+      'Authorization': 'Bearer $token',
       'Content-Type': 'application/json',
     };
+  }
+
+  String createToken() {
+    final parts = [
+      'j09j02a3jfq:3fqj2q23aeaf',
+      applicationKey,
+      DateTime.now().toIso8601String().replaceFirst(RegExp(r'\..*'), ''),
+    ];
+
+    final bytes = utf8.encode(parts.join(''));
+    final digest = sha256.convert(bytes);
+    parts.add(digest.toString());
+
+    final stringToBase64 = utf8.fuse(base64);
+    return stringToBase64.encode(parts.join('.'));
   }
 }
